9107f9f507c7adee0bc5da5b1959f9587bd4f922
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-12-12 18:07:05 +0100

    aggiunto metodo di apertura e lettura file implementando e istanziando l'oggetto Indirizzo

--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -6,5 +6,54 @@ using System.Runtime.CompilerServices;
 
 List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
 
+StreamReader file = File.OpenText("C:\\Users\\User\\Desktop\\Esercizi Generation\\ListaIndirizzi\\ListaIndirizzi\\File\\addresses.csv");
+
+file.ReadLine();
+
+
+while (!file.EndOfStream) {
+
+    string riga = file.ReadLine();
+
+    string[] informazioniIndirizzo = riga.Split(",");
+
+    if (informazioniIndirizzo.Length > 6) {
+        string nome = informazioniIndirizzo[0] + "-" + informazioniIndirizzo[1];
+        string cognome = informazioniIndirizzo[2];
+        string via = informazioniIndirizzo[3];
+        string citta = informazioniIndirizzo[4];
+        string provincia = informazioniIndirizzo[5];
+        string zip = informazioniIndirizzo[6];
+
+        Indirizzo indirizzo = new Indirizzo(nome, cognome, via, citta, provincia, zip);
+        listaIndirizzi.Add(indirizzo);
+    } else if (informazioniIndirizzo.Length < 6) {
+        string nome = informazioniIndirizzo[0];
+        string citta = informazioniIndirizzo[1];
+        string provincia = informazioniIndirizzo[2];
+        string zip = informazioniIndirizzo[3];
+
+        Indirizzo indirizzo = new Indirizzo(nome, citta, provincia, zip);
+        listaIndirizzi.Add(indirizzo);
+    } else {
+        string nome = informazioniIndirizzo[0];
+        string cognome = informazioniIndirizzo[1];
+        string via = informazioniIndirizzo[2];
+        string citta = informazioniIndirizzo[3];
+        string provincia = informazioniIndirizzo[4];
+        string zip = informazioniIndirizzo[5];
+
+        Indirizzo indirizzo = new Indirizzo(nome, cognome, via, citta, provincia, zip);
+        listaIndirizzi.Add(indirizzo);
+    }
+
+}
+
+file.Close();
+
+foreach (Indirizzo indirizzo in listaIndirizzi) {
+    Console.WriteLine(indirizzo.ToString());
+}
+
 
 
